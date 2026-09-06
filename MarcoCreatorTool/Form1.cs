a5480dced60587dce1d3afdef3fd0fee6d1c2971
Danny Thai <dtthai1@uci.edu>
2026-09-05 23:28:30 -0700

    initial commit

---
+++ MarcoCreatorTool/Form1.cs
@@ -0,0 +1,25 @@
+namespace MarcoCreatorTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void recordButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void play_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
